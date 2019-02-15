9f28cdce5ec2d0872f7a6b10c3c9d4cd6cd5b823
adial25 <Sri.AdilakshmiMulagala@dal.ca>
2019-02-15 15:26:46 -0400

    returning all the functions

---
+++ task1A.py
@@ -0,0 +1,94 @@
+import string
+
+def unique_words(filename):
+
+    hist = histogram(filename)
+    mylist = list(hist)
+    mylist =  list(dict.fromkeys(mylist))
+    return mylist
+
+def count_the_article(filename):
+
+    count = 0
+    list_2 = ["a", "the", "at", "run", "to","and","are","or","for","an","this"]
+    hist = histogram(filename)
+    for item in list_2:
+        if item in hist:
+            count += 1
+    return count
+
+def sorted_words(filename):
+
+    hist = dict()
+    file = open(filename)
+    for line in file:
+        line = line.split()
+        for word in line:
+            word = word.strip(string.punctuation + string.whitespace).lower()
+            hist[word] = len(word)
+    lst = [(value, key) for key, value in hist.items()]
+    lst.sort(reverse=True)
+    return lst
+
+
+def character_word_count(filename):
+
+    hist = dict()
+    file = open(filename)
+    for line in file:
+        line = line.split()
+        for word in line:
+            word = word.strip(string.punctuation + string.whitespace).lower()
+            hist[word] = len(word)
+    return hist
+
+def starts_with_vow(filename):
+
+    tup = ("a", "e", "i", "o", "u")
+    hist = dict()
+    file = open(filename)
+    for line in file:
+        line = line.split()
+        for word in line:
+            word = word.strip(string.punctuation + string.whitespace).lower()
+            if word[0] in tup:
+                hist[word] = hist.get(word, 0) + 1
+
+    return len(hist)
+
+def histogram(filename):
+
+    hist = dict()
+    file = open('filename')
+    for line in file:
+        line = line.split()
+        for word in line:
+            word = word.strip(string.punctuation + string.whitespace).lower()
+            hist[word] = hist.get(word, 0) + 1
+    return hist
+
+
+filename = 'Book1.txt'
+print(unique_words(filename))
+print(count_the_article(filename))
+print(sorted_words(filename))
+print(character_word_count(filename))
+print(starts_with_vow(filename))
+print("..........")
+
+
+filename = 'Book2.txt'
+print(unique_words(filename))
+print(count_the_article(filename))
+print(sorted_words(filename))
+print(character_word_count(filename))
+print(starts_with_vow(filename))
+print("..........")
+
+
+filename = 'Book3.txt'
+print(unique_words(filename))
+print(count_the_article(filename))
+print(sorted_words(filename))
+print(character_word_count(filename))
+print(starts_with_vow(filename))
